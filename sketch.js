a05b989995ce89e63532bd3321f169ed0916bf66
Maair-subzwari <70338297+Maair-subzwari@users.noreply.github.com>
2020-08-29 11:43:49 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var fixedRect;
+var movingRect;
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(300,200,50,30);
+  movingRect =  createSprite(400, 200, 50, 30);
+  fixedRect.shapeColor = "green";
+  movingRect.shapeColor = "green";
+
+} 
+
+function draw() {
+  background(255,255,255);
+  //movingRect.x = World.mouseX;
+  //movingRect.y = World.mouseY;
+  movingRect.velocityX = -2;
+  fixedRect.velocityX = 3;
+  if (movingRect.x - fixedRect.x === fixedRect.width/2+movingRect.width/2 && 
+    fixedRect.x - movingRect.x === fixedRect.width/2+movingRect.width/2 && 
+    movingRect.y - fixedRect.y === fixedRect.height/2+movingRect.height/2 && 
+    fixedRect.y - movingRect.y === fixedRect.height/2+movingRect.height/2) {
+       
+    fixedRect.shapeColor = "red";
+  movingRect.shapeColor = "red";
+  movingRect.velocityX = movingRect.velocityX*(-1);
+  fixedRect.velocityX = fixedRect.velocityX*(1);
+  }
+  else {
+    fixedRect.shapeColor = "green";
+  movingRect.shapeColor = "green";
+}
+  drawSprites();
+}
